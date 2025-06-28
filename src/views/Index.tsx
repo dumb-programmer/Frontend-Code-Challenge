1545c625cf8554a022828583828caf5946e9c790
Asad Khan <asadkhan6192@gmail.com>
2025-06-28 14:15:56 +0500

    Add links to other pages on index page

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Index(){
-    return null;
+    return <nav>
+        <ul>
+            <li className="link link-primary"><Link to="/items">Items</Link></li>
+            <li className="link link-primary"><Link to="/items/create">Add Item</Link></li>
+            <li className="link link-primary"><Link to="/checkout">Checkout</Link></li>
+        </ul>
+    </nav>;
 }
